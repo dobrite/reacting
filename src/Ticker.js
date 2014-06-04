0f3c7640ddfc958689b9505dc976d01bcabc3244
Dave O <davidobrite@gmail.com>
2014-06-03 19:42:05 -0700

    WIP

--- src/Ticker.js
+++ src/Ticker.js
@@ -6,6 +6,17 @@ var Reacting = require('reacting');
 var Ticker = React.createClass({
   mixins: Reacting.AnimatedValue('value'),
 
+  componentWillReceiveProps: function (nextProps) {
+    console.log("R", nextProps);
+    this.setProps({current: 55});
+  },
+
+  componentWillUpdate: function (nextProps) {
+    console.log("U", nextProps);
+    if (nextProps.current === 70) return;
+    //this.setProps({current: nextProps.current + 1});
+  },
+
   getDefaultProps: function () {
     return {
       current: "0"
@@ -13,6 +24,7 @@ var Ticker = React.createClass({
   },
 
   render: function () {
+    console.log("--", this.props.current, "--");
     return (
       <div>{this.props.current}</div>
     );
--- src/__tests__/Ticker-test.js
+++ src/__tests__/Ticker-test.js
@@ -1,7 +1,19 @@
 /** @jsx React.DOM */
 jest.dontMock('../Ticker.js');
 describe('Ticker', function () {
-  it('updates value', function () {
+  it('state defaults to 0 when initialized', function () {
+    var React = require('react/addons');
+    var Ticker = require('../Ticker');
+    var TestUtils = React.addons.TestUtils;
+
+    var ticker = <Ticker />;
+    TestUtils.renderIntoDocument(ticker);
+
+    var domTicker = TestUtils.findRenderedComponentWithType(ticker, Ticker);
+    expect(domTicker.getDOMNode().textContent).toEqual("0");
+  });
+
+  it('displays current when passed in', function () {
     var React = require('react/addons');
     var Ticker = require('../Ticker');
     var TestUtils = React.addons.TestUtils;
@@ -11,5 +23,16 @@ describe('Ticker', function () {
 
     var domTicker = TestUtils.findRenderedComponentWithType(ticker, Ticker);
     expect(domTicker.getDOMNode().textContent).toEqual("10");
+  });
+  it.only('displays current when passed in', function () {
+    var React = require('react/addons');
+    var Ticker = require('../Ticker');
+    var TestUtils = React.addons.TestUtils;
+
+    var ticker = <Ticker />;
+    TestUtils.renderIntoDocument(ticker);
+
+    var domTicker = TestUtils.findRenderedComponentWithType(ticker, Ticker);
+    domTicker.setProps({current: 10});
   });
 });
