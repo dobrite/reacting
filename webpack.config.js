17b269bb3dc19b30972ef936c746b030bc9c86d4
David Obrite <david@ministrycentered.com>
2014-11-28 09:23:53 -0800

    webpack dev server working

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   entry: "./src/reacting.js",
-    output: {
-        path: __dirname,
-        filename: "./js/bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.js$/, loader: "es6-loader!jsx-loader?harmony" }
-        ]
-    }
+  context: __dirname,
+  output: {
+    path: __dirname + '/public/',
+    filename: "js/bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.js$/, loader: "es6-loader!jsx-loader?harmony" }
+    ]
+  },
+  devServer: {
+    contentBase: './public/',
+  }
 };
